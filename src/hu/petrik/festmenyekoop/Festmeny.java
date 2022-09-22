4295d119159f7321e9639a29cda1ea61811dca78
Sárándi <sarandi.balint@gmail.com>
2022-09-22 08:28:33 +0200

    UML

--- src/hu/petrik/festmenyekoop/Festmeny.java
+++ src/hu/petrik/festmenyekoop/Festmeny.java
@@ -1,2 +1,56 @@
-package hu.petrik.festmenyekoop;public class Festmeny {
+package hu.petrik.festmenyekoop;
+
+import java.time.LocalDateTime;
+
+public class Festmeny {
+    private String cim;
+    private String festo;
+    private String stilus;
+    private int licitekSzama;
+    private int legmagasabbLicit;
+    private LocalDateTime legutolsoLicitIdeje;
+    private boolean elkelt;
+
+
+    public Festmeny(String cim, String festo, String stilus) {
+        this.cim = cim;
+        this.festo = festo;
+        this.stilus = stilus;
+    }
+
+    public String getFesto() {
+        return festo;
+    }
+
+    public String getStilus() {
+        return stilus;
+    }
+
+    public int getLicitekSzama() {
+        return licitekSzama;
+    }
+
+    public int getLegmagasabbLicit() {
+        return legmagasabbLicit;
+    }
+
+    public LocalDateTime getLegutolsoLicitIdeje() {
+        return legutolsoLicitIdeje;
+    }
+
+    public boolean getElkelt() {
+        return elkelt;
+    }
+
+    public void setElkelt(boolean elkelt) {
+        this.elkelt = elkelt;
+    }
+
+    public void licit(){
+
+    }
+
+    public void licit(int mertek){
+
+    }
 }
